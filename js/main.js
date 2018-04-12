e3c98c88d0373925e53d95935ce106d98c790d24
Decio Casagrande <decio.crf@gmail.com>
2018-04-12 16:48:23 -0300

    Added fancybox to show galleries

--- js/main.js
+++ js/main.js
@@ -1,6 +1,66 @@
 const look = document.getElementById('look');
 
-$('.carousel').carousel();
+$('#sdev').click( function () {
+    $('[data-fancybox="software"]').fancybox({
+        // Enable keyboard navigation
+        keyboard: true,
+    
+        btnTpl: {
+            // Arrows
+            arrowLeft:
+                '<a data-fancybox-prev class="fancybox-button fancybox-button--arrow_left" title="{{PREV}}" href="javascript:;">' +
+                '<svg viewBox="0 0 40 40">' +
+                '<path d="M18,12 L10,20 L18,28 M10,20 L30,20"></path>' +
+                "</svg>" +
+                "</a>",
+    
+            arrowRight:
+                '<a data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="{{NEXT}}" href="javascript:;">' +
+                '<svg viewBox="0 0 40 40">' +
+                '<path d="M10,20 L30,20 M22,12 L30,20 L22,28"></path>' +
+                "</svg>" +
+                "</a>"
+        },
+        
+    });
+    
+    
+});
+
+$('#art').click(function () {
+    $('[data-fancybox="artGallery"]').fancybox({
+        
+        // Enable keyboard navigation
+        keyboard: true,
+        // ProtecImages
+        protect: true,
+
+        btnTpl: {
+            // Arrows
+            arrowLeft:
+                '<a data-fancybox-prev class="fancybox-button fancybox-button--arrow_left" title="{{PREV}}" href="javascript:;">' +
+                '<svg viewBox="0 0 40 40">' +
+                '<path d="M18,12 L10,20 L18,28 M10,20 L30,20"></path>' +
+                "</svg>" +
+                "</a>",
+    
+            arrowRight:
+                '<a data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="{{NEXT}}" href="javascript:;">' +
+                '<svg viewBox="0 0 40 40">' +
+                '<path d="M10,20 L30,20 M22,12 L30,20 L22,28"></path>' +
+                "</svg>" +
+                "</a>"
+        },
+
+
+       
+        
+    });
+ 
+   
+});
+
+
 
 function onQuadrant(evt){
     
