400f12468aa82a7a2bb3413b42013f79d4bad8b6
Decio Casagrande <decio.crf@gmail.com>
2018-04-14 12:15:59 -0300

    Added 3D assets for testing

--- js/main.js
+++ js/main.js
@@ -62,6 +62,36 @@ $('#art').click(function () {
 
 
 
+$('#3dGal').click( function () {
+    
+    $('[data-fancybox="3dGallery"]').fancybox({
+        // Enable keyboard navigation
+        keyboard: true,
+    
+        btnTpl: {
+            // Arrows
+            arrowLeft:
+                '<a data-fancybox-prev class="fancybox-button fancybox-button--arrow_left" title="{{PREV}}" href="javascript:;">' +
+                '<svg viewBox="0 0 40 40">' +
+                '<path d="M18,12 L10,20 L18,28 M10,20 L30,20"></path>' +
+                "</svg>" +
+                "</a>",
+    
+            arrowRight:
+                '<a data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="{{NEXT}}" href="javascript:;">' +
+                '<svg viewBox="0 0 40 40">' +
+                '<path d="M10,20 L30,20 M22,12 L30,20 L22,28"></path>' +
+                "</svg>" +
+                "</a>"
+        },
+        
+    }); 
+    
+    
+    
+});
+
+
 function onQuadrant(evt){
     
         if ((evt.clientX >= 0 && evt.clientX <= (innerWidth / 3) ) && (evt.clientY >= 0 && evt.clientY <= (innerHeight / 2)) ){
